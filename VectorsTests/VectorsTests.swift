6d6482bcbbd68ada5da191ee71622b3828f31baa
alperduran217 <alperduran217@gmail.com>
2016-02-21 22:38:40 +0200

    Initial Commit

---
+++ VectorsTests/VectorsTests.swift
@@ -0,0 +1,36 @@
+//
+//  VectorsTests.swift
+//  VectorsTests
+//
+//  Created by Mehmet Alper Duran on 21.02.2016.
+//  Copyright © 2016 Alper Duran. All rights reserved.
+//
+
+import XCTest
+@testable import Vectors
+
+class VectorsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
